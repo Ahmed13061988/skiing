3187e73ea34c73744f33b6f7e02c6759985acb51
Ahmed Hussein <wolktheline@yahoo.com>
2020-02-05 00:49:59 +0000

    adding methods

--- lib/skiing/cli.rb
+++ lib/skiing/cli.rb
@@ -1,12 +1,11 @@
 class Skiing::CLI 
   def call 
-    
     menu 
-    list 
-   
+    list_ski
+    farewell 
   end
   
-  def list 
+  def list_ski
     puts "This is a list of the skies areas in Colorado:"
     puts "
       Arapahoe Basin
@@ -21,8 +20,21 @@ class Skiing::CLI
       Crested Butte "
   end 
    def menu 
-     puts "Please selcet the ski area you intrest in"
-     input = get.strip.downcase 
+     input = nil 
+     while input != "exit"
+     puts "Please selcet the ski area you intrest in."
+     input = gets.strip.downcase 
+     case input 
+     when"1"
+     puts "more info about ..."
+     when "2"
+     puts "more info about ..."
+     when "list"
+       list_ski 
+     end 
+   end
    end 
-  
+   def farewell 
+     puts "Have a great time in Colorado !!"
+   end 
 end 
