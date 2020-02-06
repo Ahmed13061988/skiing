4d7d3b792f60b17a15e427a467c42c9316e39229
Ahmed Hussein <wolktheline@yahoo.com>
2020-02-06 01:10:22 +0000

    adding code

--- lib/skiing/cli.rb
+++ lib/skiing/cli.rb
@@ -1,28 +1,18 @@
 
 class Skiing::CLI 
-  def call 
-    menu 
-  end
-  
-  def list_ski
-    # puts "This is a list of the skies areas in Colorado:"
-    # puts "
-    #   Arapahoe Basin
-    #   Aspen Highlands (Aspen)
-    #   Aspen Mountain (Aspen) 
-    #   Beaver Creek
-    #   Breckenridge
-    #   Buttermilk (Aspen)
-    # puts   "Chapman Hill Ski Area"
-    #   Copper Mountain
-    #   Cranor Ski Area
-    #   Crested Butte "
-       @skiies = Skiing::Ski.areas
-  end 
-   def menu 
+    
+    def call
+      menu 
+    end
+    
+    def list_ski
+        @skiies = Skiing::Ski.areas
+    end 
+      
+    def menu 
      input = nil 
      while input != "exit"
-     puts "Welcome to skiing , if you want to see the list print list "
+     puts "Welcome to skiing areas info,if you want to see the list print list or type exit to exit the program"
      input = gets.strip.downcase 
      case input 
      when"1"
@@ -30,13 +20,15 @@ class Skiing::CLI
      when "2"
      puts "more info about ..."
      when "list"
-       list_ski 
-     else 
+      list_ski 
+     when "exit"
+         farewell
+     else
        puts "Invalid input !"
      end 
-   end
+    end
    end 
-   def farewell 
+    def farewell 
      puts "Have a great time in Colorado !!"
-   end 
+    end 
 end 
